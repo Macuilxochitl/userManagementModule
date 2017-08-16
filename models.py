f76284285b6276ec9ae85e30136eb0ad5f9fcdd1
Macuilxochitl <admin@furrylop.com>
2017-08-17 03:42:13 +0800

    first commit.

---
+++ models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+
+'''
+注意!
+如果是复制到你项目自己的models，就只用复制下面这个class.
+
+'''
+
+class user(models.Model):
+	id=models.AutoField("ID",primary_key=True)
+	username=models.CharField(max_length=60,unique=True)
+	password=models.CharField(max_length=257)
+	createTime=models.DateTimeField(auto_now_add=True)
+	lastUpdateTime=models.DateTimeField(auto_now=True)
+	lastLoginTime=models.DateTimeField(auto_now=True)
+	isAdmin=models.BooleanField(default=False)
+	regIP=models.CharField(max_length=60,default="")
+
+	def __str__(self):
+		return "username="+self.username+",password="+self.password+",createTime="+self.createTime.strftime('%y-%m-%d %H:%M:%S')+",lastLoginTime="+self.lastLoginTime.strftime('%y-%m-%d %H:%M:%S')+",ip="+self.regIP
---
+++ module.py
@@ -0,0 +1,89 @@
+import hashlib
+
+def addUser(un,pw,ip=""):
+	from models import user as u
+	
+	obj=u(username=un,password=pw,regIP=ip)
+	obj.save()
+	
+def delUser(un):
+	from models import user as u
+	try:
+		obj=u.objects.get(username=un)
+		obj.delete()
+	except:
+		return False
+	return True
+
+def changePassword(username):
+	from models import user as u
+	obj=u.objects.get(username=user)
+	newpwd=sha512(newpwd)
+	obj.password=newpwd
+	obj.save()
+
+def getPasswd(un):
+	from Blog.models import user as u
+	try:
+		
+		obj=u.objects.get(username=un)
+	except:
+		return ""
+	return obj.password
+
+def isLogin(request):#如果已登录，则返回登录用户的用户名；如果没登录，返回""
+	from models import user as u
+	try:
+		un=request.COOKIES['user']
+	except:
+		un=""
+	try:
+		user=u.objects.get(username=un)
+	except:
+		return ""
+	try:
+		value=request.COOKIES['password']
+	except:
+		return ""	
+
+	if value==getKey(un):
+		return un
+	else:
+		return ""
+
+def isAdmin(request):#如果目前登录的用户是管理员，则返回True；如果目前登陆的用户不是管理员或没登录，则返回False
+	from models import user as u
+	user=isLogin(request)
+	try:
+		user=u.objects.get(username=user)
+	except:
+		return False
+	return user.isAdmin
+
+def auth(username,passwd):
+	pw=getPasswd(username)
+	if pw==passwd and passwd!="":
+		return True
+	else:
+		return False
+
+def setCookie(request,username,jumpTo):
+	r=render(request,jumpTo)
+	r.set_cookie("user",username)
+	r.set_cookie("password",getKey(username))
+	return r
+
+def logout(request,jumpTo):
+	r=render(request,jumpTo)
+	r.set_cookie("user","")
+	r.set_cookie("password","")
+	return r
+
+def getKey(username):
+	s=username+getPasswd(username);
+	return sha512(s)
+
+def sha512(str):
+	m=hashlib.sha512()
+	m.update(str.encode("utf-8"))
+	return m.hexdigest()
